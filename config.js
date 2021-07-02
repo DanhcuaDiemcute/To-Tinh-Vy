654ce4cc6de86edcdff6ea41abb9709325c6bf96
yeungan2002 <86814587+yeungan2002@users.noreply.github.com>
2021-07-02 10:47:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    introTitle: 'Bé Ngân Cute uii ><',
+    introDesc: `Nếu bà biết tui thích bà từ lâu vậy trả lời dùm tui câu hỏi này nhó ><`,
+    btnIntro: 'Dạ anh Shen pro',
+    title: 'Vũ trụ thì bao la một màu đen huyền bí. Còn vũ trụ anh bé tí chỉ gói gọn là Ngân ><',
+    desc: 'Bà có thích tui hông nàaaaaa ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
@@ -15,5 +12,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/shenproplayer'
 }
