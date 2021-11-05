0df638d7fb460dc759b2498a3fd0538d9fb91b08
DanhcuaDiemcute <93750721+DanhcuaDiemcute@users.noreply.github.com>
2021-11-05 09:58:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Vy bae cute uiii ><',
-    introDesc: `Nếu bà biết tui thích bà từ lâu vậy trả lời dùm tui câu hỏi này nhoaaaaa`,
-    btnIntro: 'Vângggg Triều >< ',
-    title: 'Vũ trụ thì bao la huyền bí. Vũ trụ của tui thì bé tí gói gọn là Vy ><',
-    desc: 'Vậy bà có thích tui hông >< ',
+    introTitle: 'Diễm bae cute uiii ><',
+    introDesc: `Nếu bé không phiền thì trả lời dùm anh câu hỏi này nhoaaaaa`,
+    btnIntro: 'Vângggg  >< ',
+    title: 'Vũ trụ thì bao la huyền bí. Vũ trụ của tui thì bé tí gói gọn là Diễm ><',
+    desc: 'Vậy em có thích  hông >< ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Đừng dỗi anh nữa ó',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/trieu.nguyennhat.31'
+    messLink: 'https://www.facebook.com/shenproplayer'
 }
